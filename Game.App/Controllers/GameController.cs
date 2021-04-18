bb354bcef95c5c6abd3a34157c5c4dee09a45135
Miguel dos Santos Ribeiro <migueldossantos011@gmail.com>
2021-04-18 15:38:32 -0300

    V1 Consumindo eota Get ListarGames

--- Game.App/Controllers/GameController.cs
+++ Game.App/Controllers/GameController.cs
@@ -1,6 +1,7 @@
 ﻿using Game.App.Models;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Net.Http;
 using System.Threading.Tasks;
@@ -13,15 +14,17 @@ namespace Game.App.Controllers
         public async Task<IActionResult> Index()
         {
             var games = new List<GameModel>();
-            using (var httpClient = new HttpClient())
+            using (var client = new HttpClient())
             {
-                using (var response = await httpClient.GetAsync("http://localhost:6000/api/game/"))
+                using (var response = await client.GetAsync("http://localhost:6000/api/game"))
                 {
                     var apiResponse = await response.Content.ReadAsStringAsync();
-                    var json = JsonConvert.DeserializeObject<List<GameModel>>(apiResponse);
+                    games = JsonConvert.DeserializeObject<List<GameModel>>(apiResponse);
                 }
             }
             return View(games);
+
+
         }
     }
 }
