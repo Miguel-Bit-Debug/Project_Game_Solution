c9fa256118ca55a606fa363d69b8ba5293e1a077
Miguel-Bit-Debug <migueldossantos011@gmail.com>
2021-05-12 19:13:47 -0300

    isolando metodos da controller

---
+++ Games.API/Controllers/CreateController.cs
@@ -0,0 +1,30 @@
+﻿using Game.API.Models;
+using Game.API.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Games.API.Controllers
+{
+    public class CreateController : Controller
+    {
+        private readonly IGameRepository<GameModel> _gameRepository;
+
+        public CreateController(IGameRepository<GameModel> gameRepository)
+        {
+            _gameRepository = gameRepository;
+        }
+
+        public IGameRepository<GameModel> GameRepository => _gameRepository;
+
+        [HttpPost("api/game")]
+        public IActionResult Create([FromBody] GameModel game)
+        {
+            if (game == null)
+            {
+                return BadRequest();
+            }
+
+            GameRepository.AdicionarGame(game);
+            return Created("", game);
+        }
+    }
+}
---
+++ Games.API/Controllers/UpdateController.cs
@@ -0,0 +1,38 @@
+﻿using Game.API.Models;
+using Game.API.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace Games.API.Controllers
+{
+    public class UpdateController : Controller
+    {
+        private readonly IGameRepository<GameModel> _gameRepository;
+
+        public UpdateController(IGameRepository<GameModel> gameRepository)
+        {
+            _gameRepository = gameRepository;
+        }
+
+        [HttpPut("api/game/{id}")]
+        public IActionResult Update(Guid id, [FromBody] GameModel game)
+        {
+            var gameAntigo = _gameRepository.ObterGameById(id);
+            if (gameAntigo == null)
+            {
+                return BadRequest();
+            }
+
+            gameAntigo.Nome = game.Nome;
+            gameAntigo.Descricao = game.Descricao;
+            gameAntigo.MaiorDeIdade = game.MaiorDeIdade;
+            gameAntigo.Multiplayer = game.Multiplayer;
+            gameAntigo.MaiorDeIdade = game.MaiorDeIdade;
+            gameAntigo.DataLancamento = game.DataLancamento;
+
+            _gameRepository.AlterarGame(gameAntigo);
+
+            return Ok(game);
+        }
+    }
+}
