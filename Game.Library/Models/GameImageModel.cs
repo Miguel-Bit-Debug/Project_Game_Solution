608f15a56bf40f6e5b848756fd37c0bf0e83016f
Miguel-Bit-Debug <migueldossantos011@gmail.com>
2021-09-04 19:27:58 -0300

    Adicionando metodos awaiters

---
+++ Game.Library/Models/GameImageModel.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace Game.Library.Models
+{
+    public class GameImageModel : GameBaseModel
+    {
+        public string Name { get; set; }
+        public IFormFile Image { get; set; }
+    }
+}
