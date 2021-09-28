411ee25a32dd46d6c47569d8e215c5ff10b0eb09
Miguel-Bit-Debug <migueldossantos011@gmail.com>
2021-09-27 22:19:21 -0300

    changes

---
+++ We.Application/Game.App/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Game.Library.Models;
+using System.Net.Http;
+using Newtonsoft.Json;
+
+namespace Game.App.Controllers
+{
+    public class HomeController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            var games = new List<GameModel>();
+            using (var httpClient = new HttpClient())
+            {
+                using (var response = await httpClient.GetAsync("http://localhost:5000/api/game"))
+                {
+                    var apiResponse = await response.Content.ReadAsStringAsync();
+                    games = JsonConvert.DeserializeObject<List<GameModel>>(apiResponse);
+                }
+            }
+            return View(games);
+        }
+    }
+}
---
+++ We.Application/Games.API/Controllers/ListImagesController.cs
@@ -0,0 +1,28 @@
+﻿using Game.API.Repositories;
+using Game.Library.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Games.API.Controllers
+{
+    [Route("api/game")]
+    public class ListImagesController : Controller
+    {
+        private readonly IGenericRepository<ImageModel> _context;
+
+        public ListImagesController(IGenericRepository<ImageModel> context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("images")]
+        public IActionResult Index()
+        {
+            var listImg = _context.List().Select(x => x.Data).ToList();
+            return Ok(listImg);
+        }
+    }
+}
