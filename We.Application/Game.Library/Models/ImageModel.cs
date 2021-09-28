bfcd69fa5c4c25a0fee8bf46e823a13b0f94a8f2
Miguel-Bit-Debug <migueldossantos011@gmail.com>
2021-09-27 22:20:28 -0300

    changes

--- We.Application/Game.Library/Models/ImageModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Game.Library.Models
-{
-    public class ImageModel : BaseModel
-    {
-        public string Name { get; set; }
-        public byte[] Data { get; set; }
-        public string ContentType { get; set; }
-    }
-}
